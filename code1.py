ea652699e30f18ea6efe6257c0edce78e042cf0d
XxxalbertxxX <albertmatosm007@gmail.com>
2024-11-25 20:45:50 -0400

    Code added to the bot

--- code1.py
+++ code1.py
@@ -1,1 +1,34 @@
-print("Hola mundo")
+"This codes are for discord chatbot"
+
+import discord
+from discord.ext import commands
+import random
+import time
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='|', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+"thas all i know"
