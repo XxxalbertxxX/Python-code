943223441081436e317a9bb5b81fb280665f4d72
XxxalbertxxX <albertmatosm007@gmail.com>
2024-12-05 17:02:48 -0400

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,13 +1,13 @@
-"This codes are for discord chatbot"
 
 import discord
 from discord.ext import commands
 import random
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='|', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -29,5 +29,108 @@ async def add(ctx, left: int, right: int):
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
     await ctx.send(random.choice(choices))
+    
+@bot.command()
+async def mem(ctx):
+    with open('Mis imágenes/MEME1.jpeg', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en estba variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
 
-"thas all i know"
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def mem3(ctx):
+    with open('Mis imágenes/imagen.jpeg', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en estba variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem2(ctx):
+    with open('Mis imágenes/MEM3.jpeg', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en estba variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+@bot.command()
+async def poke(ctx,arg):
+    try:
+        pokemon = arg.split(" ",1)[0].lower()
+        result = requests.get("https://pokeapi.co/api/v2/pokemon/"+pokemon)
+        if result.text == "Not Found":
+            await ctx.send("Pokemon no encontrado")
+        else:
+            image_url = result.json()["sprites"]["front_default"]
+            print(image_url)
+            await ctx.send(image_url)
+    except Exception as e:
+        print("Error:", e)
+@poke.error
+async def error_type(ctx,error):
+    if isinstance(error,commands.errors.MissingRequiredArgument):
+        await ctx.send("Tienes que darme un pokemon")
+     
+@bot.command()
+async def limpiar(ctx):
+    await ctx.channel.purge()
+    await ctx.send("Mensajes eliminados", delete_after = 3)
+
+@bot.command()
+async def contaminacion(ctx):
+    await ctx.send(f"""Hola, soy un bot {bot.user}!""")# esta linea saluda
+    await ctx.send(f'Te voy hablar un poco sobre la contaminacion')
+    await ctx.send(f'La contaminación es un gran problema a nivel mundial, muchos paises sufren a diario')
+    # Enviar una pregunta al usuario
+    await ctx.send("Quieres consejos sobre cómo combatir la contaminación? Responde con 'sí' o 'no'.")
+# Esperar la respuesta del usuario
+    def check(message):
+        return message.author == ctx.author and message.channel == ctx.channel and message.content in ['sí', 'si', 'no']
+    response = await bot.wait_for('message', check=check)
+    if response:
+        if response.content in ['sí', 'si']:
+            await ctx.send("1. No arrojar basura en los rios")
+            await ctx.send("2. Dejar de quemar basuras")   
+        else:
+            await ctx.send("Está bien, si alguna vez necesitas consejos, no dudes en preguntar.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+    await ctx.send("Quieres saber la definicion de contaminacion, responde si o no")
+    response1 = await bot.wait_for('message', check=check)
+    if response1:
+        if response1.content in ['sí', 'si']:
+            await ctx.send("la definicion de contaminacion es contaminar lugares de material organica osea que se pudran y arruinen esos lugares")    
+        else:
+            await ctx.send("Está bien, si alguna vez necesitas consejos, no dudes en preguntar.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+
+def get_perros_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('perros')
+async def perros(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_perros_image_url()
+    await ctx.send(image_url)
+
+bot.run("TOKEN REDACTED")
